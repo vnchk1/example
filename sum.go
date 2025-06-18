918d667e0d8a874edbd60173448e39000c638bd9
Ivan Cherviakov <ivanchervyakov2905@gmail.com>
2025-06-18 22:22:24 +0300

    New main as bugFix without last Println

--- sum.go
+++ sum.go
@@ -2,10 +2,9 @@ package main
 
 import "fmt"
 
-func sum(n,m int) int {
-<<<<<<< HEAD
-	return (n + m) / 2
+func sum(n,m int) float64 {
+	return (n + m) / 3
 }
 func main() {
-	fmt.Println(sum(2, 3))
+	fmt.Println(sum(4, 4))
 }
